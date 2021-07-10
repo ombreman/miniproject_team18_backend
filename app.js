d75e85beaf4fb6569265bf98a304c24e2744b689
Changgil Jeong <ombreman21@gmail.com>
2021-07-10 15:15:03 +0100

    add initial files

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require("express");
-const { sequelize } = require("./models/index")
+const { sequelize, User } = require("./models/index")
 
 const app = express();
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 
-app.get('/', (req, res) => {res.send("<h1>Hello!!!</h1>")})
+app.get('/', async (req, res) => {
+    const newUser = await User.create({
+        userId: "nana",
+        nickname: "gil",
+        password: "1234"
+    })
+    res.send(newUser)
+})
 
 sequelize.sync({ force: false })
     .then(() => console.log("Connected to MySQL."))
     .catch((err) => console.error(err))
 
-
 app.listen(8080, () => {
-  console.log("서버가 켜졌어요!");
-}); 
-
-
-
-
+    console.log("서버가 켜졌어요!");
+});
--- models/index.js
+++ models/index.js
@@ -1,12 +1,16 @@
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize') // 라이브러리를 불러왔다.
 const env = process.env.NODE_ENV || 'development'
 const config = require(__dirname + '/../config/config.json')[env]
+const User = require('./users')
+
 const sequelize = new Sequelize(
   config.database,
   config.username,
   config.password,
   config
 )
+
 const db = {}
 db.sequelize = sequelize
+db.User = User( sequelize, Sequelize.DataTypes)
 module.exports = db
