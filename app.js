86e08b2c40cf7c13936d55953e101457a500bc99
Changgil Jeong <ombreman21@gmail.com>
2021-07-12 14:11:16 +0100

    Add main board page, category filter

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { sequelize } = require("./models/index")
+const { sequelize, Ad, User, Comment } = require("./models/index")
 
 // Express Settings
 const app = express()
@@ -7,10 +7,29 @@ const port = 8080
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Main Board Page
+app.get('/ads', async (req, res) => {
+    const ads = await Ad.findAll();
+    console.log(ads);
+    res.json(ads);
+});
+
+//Main Board Page Filter
+app.get('/ads', async (req, res) => {
+    const { category } = req.query;
+    if (category) {
+        const ads = await Ad.findAll({ where: {category: category} });
+        res.json(ads);
+    } else {
+        const ads = await Ad.findAll();
+        res.json(ads);
+    }
+});
+
 // Sequelize Connected
 sequelize.sync({ force: false })
     .then(() => console.log("Connected to MySQL."))
     .catch(err => console.error(err))
 
 // Port 8080
-app.listen(port, port => console.log("Server is running on port " + port))
+app.listen(port, () => console.log("Server is running on port " + port))
