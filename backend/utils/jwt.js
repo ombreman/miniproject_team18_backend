4dc4240067f53fc2badc28e3d22fba5d66ceca7c
hayasha <73967871+hayasha@users.noreply.github.com>
2021-07-15 19:53:09 +0900

    Delete backend directory

--- backend/utils/jwt.js
+++
@@ -1,5 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-module.exports = accountId => {
-    return jwt.sign({ userInfo: accountId }, "my-secret-key", { expiresIn: '300s' })
-}
--- backend/utils/moment.js
+++
@@ -1,5 +0,0 @@
-const moment = require("moment");
-
-module.exports = model => {
-    return moment(model.getDataValue('createdAt')).format('YYYY MMMM DD h mm ss a')
-}
